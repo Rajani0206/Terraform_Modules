5f224a2a64f45ddc03fc7e79c24ffdd23b34b43e
Rajani0206 <rajanisc15@gmail.com>
2025-03-09 16:51:56 +0530

    Update variables.tf

--- EC2_Module/variables.tf
+++ EC2_Module/variables.tf
@@ -1,12 +1,14 @@
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
   
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  default     = "t2.micro"
 }
 
 variable "ami_id" {
